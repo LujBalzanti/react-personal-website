17898361fa93f7e3d2e24de6edf1def3f182e7b2
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-20 12:10:09 +0000

    feat: add About page

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Components.css'
+
+function About() {
+    return (
+      <div className="about">
+          <h1 class="light-text">About me</h1>
+          <p class="light-text">This is about me</p>
+      </div>
+    );
+  }
+  export default About;
