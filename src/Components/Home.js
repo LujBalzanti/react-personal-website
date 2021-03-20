25fec14dfaa3df292087e941df03caaba2dc66aa
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-20 13:17:47 +0000

    feat: Add homepage content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Components.css'
 
 function Home() {
     return (
       <div className="home">
-          <h1 class="light-text">About this website</h1>
-          <p class="light-text">This is about this website</p>
+        <div class="in-middle">
+            <h1 class="light-text-emphasis text-center">Welcome to my web development portfolio!</h1>
+            <p class="light-text main-body text-center">  
+              I'm Luj Balzanti. I decided to make this website to tell you <Link to="/about">who I am</Link>, 
+              as well as to showcase my <Link to="/projects">projects</Link> and <Link to="/skills">skills</Link>.
+            </p>
+        </div>
       </div>
     );
   }
