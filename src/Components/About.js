24357da60084e0380e31ba8e12102b3daadad529
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-20 14:32:55 +0000

    feat: Add about content

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Components.css'
 
 function About() {
     return (
       <div className="about">
-          <h1 class="light-text">About me</h1>
-          <p class="light-text">This is about me</p>
+        <div class="container col-6">
+          <h1 class="light-text-emphasis text-center">My name is Luj Balzanti. Nice to meet you!</h1>
+            <p class="light-text main-body text-center"> 
+              I was born in Slovenia in 1996 and I currently work as a freelance full stack web developer, based in London. I also enjoy weightlifting and playing a good board game in my free time.
+            </p>
+            <p class="light-text main-body text-center">
+              I greatly enjoy learning about web development and constantly look to expand my knowledge by challenging myself with katas and projects. Oftentimes, I will look to create projects with completely different frameworks, so that I become more adaptable to whatever technology I will need to use.
+            </p>
+            <p class="light-text main-body text-center">
+              I am a driven self-starter, having moved to the UK in March of 2020, I spent the majority of the dreaded COVID-19 lockdown learning about web development and programing in general. I finished <a href="https://www.edx.org/course/cs50s-introduction-to-computer-science">CS50’s Introduction to Computer Science</a> and <a href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript">CS50’s Web Programming with Python and JavaScript</a>, the Harvard Courses offered on edx.com. Since then, I’ve been working as a freelance web developer as well as working on personal projects expanding my skillset.
+            </p>
+            <p class="light-text main-body text-center">
+              Before moving to London, I worked as a mobile phone and tablet repair technician, working in a small team environment. I acquired efficient communication skills, as the day-to-day work required precise communication with both coworkers and customers. The work was very detail oriented and fast paced. Learning new skills and being adaptable was a daily requirement, as new phone models and repair techniques were introduced.
+            </p>
+            <p class="light-text main-body text-center">  
+              Outside of work, I used to be a competitive video game player. I reached the top 0.1% of a given player base in multiple video games, including League of Legends, Hearthstone, Gwent and Prismata, where I also won a world cup. I strongly enjoy games which are strategic and force me to think fast. Including playing Chess almost exclusively in blitz and bullet time modes.
+            </p>
+            <p class="light-text main-body text-center"> 
+              Besides video games, I also enjoy modern board games, weightlifting, cooking, baking, drinking coffee and owning a cat. I would love to also have a Samoyed in the future.
+            </p>
+        </div>
       </div>
     );
   }
