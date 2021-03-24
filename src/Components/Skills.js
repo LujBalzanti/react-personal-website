805f56c3354d0ad50b3d4ca1bd1ae654f6dc4f24
LujBalzanti <Lujrbalzanti@hotmail.com>
2021-03-24 21:22:08 +0000

    feat: add Skill page styling and formating

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -3,55 +3,48 @@ import './Components.css';
 
 function Skills() {
     return (
-      <div className="skills" class="container">
-          <div class="row">
-            <div class="col title-top">
+      <div className="skills" class="container-fluid">
+          <div class="row justify-content-lg-center">
+            <div class="col-lg-2 title-top">
               <div class="card nav-bg-color">
-                <div class="card-body">
-                  <h3 class="card-title light-text-emphasis">Programing Languagues</h3>
-                </div>
                   <ul class="list-group list-group-flush">
-                    <li class="list-group-item nav-bg-color light-text">Python</li>
-                    <li class="list-group-item nav-bg-color light-text">Javascript</li>
-                    <li class="list-group-item nav-bg-color light-text">Typescript</li>
+                    <li class="list-group-item bright-bg-color gray-text-emphasis text-center">Programming Languagues</li>
+                    <li class="list-group-item nav-bg-color light-text"><i class="devicon-python-plain"></i> Python</li>
+                    <li class="list-group-item nav-bg-color light-text"><i class="devicon-javascript-plain"></i> Javascript</li>
+                    <li class="list-group-item nav-bg-color light-text"><i class="devicon-typescript-plain"></i> Typescript</li>
                     <li class="list-group-item nav-bg-color light-text">SQL</li>
-                    <li class="list-group-item nav-bg-color light-text">C</li>
+                    <li class="list-group-item nav-bg-color light-text"><i class="devicon-c-plain"></i> C</li>
                   </ul>
               </div>
             </div>
-            <div class="col title-top">
+            <div class="col-lg-2 title-top">
               <div class="card nav-bg-color">
-                <div class="card-body">
-                  <h3 class="card-title light-text-emphasis">Tools & Technologies</h3>
-                </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item nav-bg-color light-text">Django</li>
-                    <li class="list-group-item nav-bg-color light-text">React</li>
-                    <li class="list-group-item nav-bg-color light-text">Html5</li>
-                    <li class="list-group-item nav-bg-color light-text">CSS3</li>
-                    <li class="list-group-item nav-bg-color light-text">Git</li>
-                    <li class="list-group-item nav-bg-color light-text">Flask</li>
-                    <li class="list-group-item nav-bg-color light-text">PostgreSQL</li>
-                    <li class="list-group-item nav-bg-color light-text">SQLite3</li>
-                  </ul>
+                  <li class="list-group-item bright-bg-color gray-text-emphasis text-center">Tools & Technologies</li>
+                  <li class="list-group-item nav-bg-color light-text"><svg viewBox="0 0 128 128" class="big-icon">
+                  <g fill="#ffffff"><path d="M90.9 61.6l.1-2v-.6h-.8c-1.2 0-2.5.3-3.4.9-1.4.8-2.2 2.3-2.2 4.4 0 3 1.5 4.7 4.1 4.7.8 0 1.3-.1 2.3-.4v-4.3l-.1-2.7zM16.3 64.2c0 3.2 1.6 4.9 4.7 4.9.7 0 1 0 2-.2v-9.5c-1-.2-1.2-.3-2-.3-3 0-4.7 1.9-4.7 5.1zM45.9 68.1c0 1.2.7 1.7 2.3 1.7.9 0 1.8-.1 2.8-.3v-4c-4 .4-5.1 1-5.1 2.6zM108.9 59c-2.3 0-3.7 1.9-3.7 5.2 0 3.3 1.3 5.2 3.7 5.2 2.3 0 3.6-1.9 3.6-5.2 0-3.2-1.3-5.2-3.6-5.2zM126 43.9c0-3.8-3.1-6.9-6.9-6.9h-111.2c-3.8 0-6.9 3.1-6.9 6.9v40.1c0 3.9 3.1 7 6.9 7h111.1c3.8 0 6.9-3.1 6.9-6.9v-40.2zm-115.7 20.4c0-6 4-10 10.2-10 1 0 1.6.1 2.6.3v-8.6h6v27c-3 .6-5.3.8-7.7.8-7.3 0-11.1-3.2-11.1-9.5zm27.7 4.2c0 4.7-.2 7-1.2 9-1 1.9-2.1 3.1-4.7 4.4l-5.5-2.6c2.6-1.2 3.7-2.3 4.5-4 .8-1.7.9-3.7.9-8.8v-11.5h6v13.5zm0-16.5h-6v-6h6v6zm18 21.3c-2 .4-5.8.7-8.3.7-5 0-7.1-1.7-7.1-5.6 0-4.2 3.3-6.1 10.3-6.7v-1.3c0-1.1-.5-1.5-2-1.5-2.2 0-5 .6-7 1.8v-4.6c3-1.2 5.1-1.8 7.8-1.8 3 0 4.8.8 5.7 2.4.5.9.5 2 .5 4.5v12.1zm21 .7h-6v-11.8c0-2.3-1-3.1-3.1-3.1-.8 0-1.9.1-2.9.4v14.5h-6v-18.3c4-.9 6.5-1.3 9.4-1.3 3 0 5.4.7 6.7 2 1.3 1.3 1.9 2.6 1.9 5.6v12zm19.3-11.6.7 5.6v1.7c0 5.1-.6 7.5-1.9 9.4-1.8 2.9-5.1 4.3-9.7 4.3-2.3 0-4.5-.3-6.5-1.2v-5.5c2 1.1 4.2 1.6 6.4 1.6 3.9 0 5.6-1.6 5.6-5.3v-.1c-1 .6-2.4.8-3.9.8-5.2 0-8.5-3.4-8.5-8.8 0-6.7 4.9-10.5 13.5-10.5 2.5 0 4.9.3 7.7.8l-2.3 4.3c-1.6-.3 1.3 0-.7-.2m12.1 14.8c-5.9 0-9.6-3.7-9.6-9.7 0-6.2 3.8-10.1 9.8-10.1 5.9 0 9.5 3.7 9.5 9.8.1 6.1-3.7 10-9.7 10z"></path></g></svg> Django</li>
+                  <li class="list-group-item nav-bg-color light-text"><i class="devicon-react-original"></i> React</li>
+                  <li class="list-group-item nav-bg-color light-text"><i class="devicon-html5-plain"></i> Html5</li>
+                  <li class="list-group-item nav-bg-color light-text"> <i class="devicon-css3-plain"></i> CSS3</li>
+                  <li class="list-group-item nav-bg-color light-text"><i class="devicon-git-plain"></i> Git</li>
+                  <li class="list-group-item nav-bg-color light-text"> <i class="devicon-flask-original"></i> Flask</li>
+                  <li class="list-group-item nav-bg-color light-text"><i class="devicon-postgresql-plain"></i> PostgreSQL</li>
+                  <li class="list-group-item nav-bg-color light-text">SQLite3</li>
+                </ul>
               </div>
             </div>
-          </div>
-          <div class="row justify-content-center title">
-            <div class="col-lg-6">
+            <div class="col-lg-5 title">
               <div class="card nav-bg-color">
-                <div class="card-body">
-                  <h3 class="card-title light-text-emphasis">Skills</h3>
-                </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item nav-bg-color light-text text-left">• Strong fundamentals of object-oriented design</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Clear and efficient communication</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Familiarity with Agile design principles</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Good under pressure - (Evidenced by experience in high stress competitive environments)</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Time management</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Comfortable in multidisciplinary and diverse environments</li>
-                    <li class="list-group-item nav-bg-color light-text text-left">• Public speaking - (Awarded "Best Presentation" for <a href="https://lokar.fmf.uni-lj.si/www/rom_konferenca/konferenca_2017/orodja/LujRomanBalzanti.html"><i>Calculating the probabilty in Poker, Texas Hold'em</i></a> - University of Ljubljana, Faculty of Mathematics and Physics - 2017)</li>
-                  </ul>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item bright-bg-color gray-text-emphasis text-center">Skills</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Strong fundamentals of object-oriented design</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Clear and efficient communication</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Familiarity with Agile design principles</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Good under pressure - (Evidenced by experience in high stress competitive environments)</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Time management</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Comfortable in multidisciplinary and diverse environments</li>
+                  <li class="list-group-item nav-bg-color light-text text-left">• Public speaking - (Awarded "Best Presentation" for <a href="https://lokar.fmf.uni-lj.si/www/rom_konferenca/konferenca_2017/orodja/LujRomanBalzanti.html"><i>Calculating the probabilty in Poker, Texas Hold'em</i></a> - University of Ljubljana, Faculty of Mathematics and Physics - 2017)</li>
+                </ul>
               </div>
             </div>
           </div>
