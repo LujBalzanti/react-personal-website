172edf298ebad201e6adae3c11730e41b7e8f7ca
LujBalzanti <Lujrbalzanti@hotmail.com>
2022-09-26 10:31:06 +0100

    feat: updates 2022

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,19 +7,19 @@ function About() {
         <div class="container-fluid col-lg-6">
           <h1 class="light-text-emphasis title text-center">My name is Luj Balzanti. Nice to meet you!</h1>
             <p class="light-text main-body text-left"> 
-              I was born in Slovenia in 1996 and I currently work from London as a Full Stack Web Developer. 
+              I was born in Slovenia in 1996 and I currently work from the UK as a Junior Software Engineer. 
             </p>
             <p class="light-text main-body text-left">
-              I am a fast learner and a driven self-starter. I moved to the UK in March 2020 and spent the majority of the COVID-19 lockdown deepening my knowledge of web development and programming in general. I finished <a href="https://www.edx.org/course/cs50s-introduction-to-computer-science">CS50’s Introduction to Computer Science</a> and <a href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript">CS50’s Web Programming with Python and JavaScript</a>, the Harvard Courses offered on edx.com. Since then, I've been working as a freelance web developer, as well as working on personal projects that help me expand my skillset.</p>
+              I am a fast learner and a driven self-starter. I moved to the UK in March 2020 and spent the COVID-19 lockdown deepening my knowledge of Software Engineering. I finished <a href="https://www.edx.org/course/cs50s-introduction-to-computer-science">CS50’s Introduction to Computer Science</a> and <a href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript">CS50’s Web Programming with Python and JavaScript</a>, the Harvard Courses offered on edx.com.</p>
             <p class="light-text main-body text-left">
-              I greatly enjoy learning and constantly look to expand my knowledge with new challenges. Oftentimes, I will look to work with frameworks I'm not yet familiar with as a way of learning them - I will try them out on personal projects or on software katas.
+              I greatly enjoy learning and constantly look to expand my knowledge with new challenges. Oftentimes, I will look to work with frameworks I'm not yet familiar with - I will try them out on personal projects or on software katas.
             </p>
             <p class="light-text main-body text-left">
-              Before I moved to London, I worked in a small team environment as a mobile phone and tablet repair technician. This required fast-paced, detail-oriented and precise communication with both coworkers and customers, skills that translate fully into agile workplace environments. As new phone models and repair techniques were introduced, I always had to adapt on the fly to current industry standards, a must-have ability in the world of programming.             </p>
+              I am logic driven and adept at working under pressure. I am good at fast-paced, detail-oriented and precise communication.</p>
             <p class="light-text main-body text-left">  
-              Besides my work experience, I used to be a competitive video game player: I reached the top 0.1% of players in multiple video games, including League of Legends, Hearthstone, Gwent, and Prismata, where I also won a world cup. I strongly enjoy games which are strategic and force me to think fast.             </p>
+              I won a World Championship for Prismata, and I also reached the top 0.1% of players in multiple games, including League of Legends, Hearthstone, and Gwent. I strongly enjoy games which are strategic and force quick decision making.</p>
             <p class="light-text main-body text-left"> 
-              My personal interests go well beyond video games, though -- I also enjoy modern board games, weightlifting, cooking, baking, drinking coffee and owning a cat. I also plan on getting a Samoyed in the near future.
+              My personal interests go well beyond games, though - I also enjoy weightlifting, hiking, cooking, baking, coffee and owning a cat. I plan on getting a Samoyed in the near future.
             </p>
         </div>
       </div>
