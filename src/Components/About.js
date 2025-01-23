34aa13a7a38b6523501cc284ab9d6dc171655f36
LujBalzanti <lujrbalzanti@gmail.com>
2025-01-23 16:46:49 +0000

    update about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ function About() {
         <div class="container-fluid col-lg-6">
           <h1 class="light-text-emphasis title text-center">My name is Luj Balzanti. Nice to meet you!</h1>
             <p class="light-text main-body text-left"> 
-              I was born in Slovenia in 1996 and I currently work from the UK as a Junior Software Engineer. 
+              I was born in Slovenia in 1996 and I currently work from the UK as a Software Engineer at ZTP. 
             </p>
             <p class="light-text main-body text-left">
               I am a fast learner and a driven self-starter. I moved to the UK in March 2020 and spent the COVID-19 lockdown deepening my knowledge of Software Engineering. I finished <a href="https://www.edx.org/course/cs50s-introduction-to-computer-science">CS50’s Introduction to Computer Science</a> and <a href="https://www.edx.org/course/cs50s-web-programming-with-python-and-javascript">CS50’s Web Programming with Python and JavaScript</a>, the Harvard Courses offered on edx.com.</p>
